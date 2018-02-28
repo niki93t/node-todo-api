a1912c1c965d22b6521258e2dcf36a412c32b9f8
Nikola Adzic <nikola@Nikolas-MacBook-Pro.local>
2018-02-28 21:27:02 +0100

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to MongoDB server");
+    }
+
+    console.log("Connected to MongoDB server");
+
+    // db.collection("Todos")
+    //     .find({
+    //         // completed: true
+    //         _id: new ObjectID("5a970477f0104882a5f71a5a")
+    //     })
+    //     .toArray()
+    //     .then((docs) => {
+    //         console.log("Todos");
+    //         console.log(JSON.stringify(docs, undefined, 2));
+    //     }, (err) => {
+    //         console.log("Unable to fetch todos", err);
+    //     });
+
+    // db.collection("Todos")
+    //     .find()
+    //     .count()
+    //     .then((count) => {
+    //         console.log("Todos");
+    //         console.log(`Count: ${count}`)
+    //     }, (err) => {
+    //         console.log("Unable to fetch todos", err);
+    //     });
+
+    //db.close();
+});
