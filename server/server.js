72eca67ff7510e3a2b1acb6ae7f484853d2d9f43
Nikola Adzic <nikola@Nikolas-MacBook-Pro.local>
2018-03-07 21:40:28 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ app.delete("/todos/:id", (req, res) => {
 
 app.patch("/todos/:id", (req, res) => {
     var id = req.params.id;
-    var body = _.pick(req.body, ["text", "completed"])
+    var body = _.pick(req.body, ["text", "completed"]);
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
@@ -101,6 +101,22 @@ app.patch("/todos/:id", (req, res) => {
             res.send({todo});
         }, (err) => {
             res.status(400).send();
+        });
+});
+
+app.post("/users", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"])
+    var user = new User(body);
+
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            res.header("x-auth", token).send(user);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
         });
 });
 
