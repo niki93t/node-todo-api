732c7dcc12c485aedefd7ffa632055fd8f6d0a7d
Nikola Adzic <nikola@Nikolas-MacBook-Pro.local>
2018-03-11 17:22:28 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.patch("/todos/:id", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-    var body = _.pick(req.body, ["email", "password"])
+    var body = _.pick(req.body, ["email", "password"]);
     var user = new User(body);
 
     user.save()
@@ -118,6 +118,21 @@ app.post("/users", (req, res) => {
         })
         .catch((err) => {
             res.status(400).send(err);
+        });
+});
+
+app.post("/users/login", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"]);
+
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthToken()
+                .then((token) => {
+                    res.header("x-auth", token).send(user);
+                });
+        })
+        .catch((err) => {
+            res.status(400).send();
         });
 });
 
